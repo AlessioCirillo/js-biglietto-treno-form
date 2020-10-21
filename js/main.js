9690b25314de6e02da83b157d901454c7c0fc115
alessio <alessio.cirillo.1995@gmail.com>
2020-10-21 16:36:39 +0200

    generazione biglietto

--- js/main.js
+++ js/main.js
@@ -22,8 +22,66 @@ bottoneGenera.addEventListener('click', function (){
     //FASCIA ETA
     var eta = document.getElementById('eta').value;
     console.log('anni', eta);
+
+    //COSTO BIGLIETTO
+    var tasso = 0.21;
+    var costo = tasso * km;
+    var offerta = 'Standard'
+
+    //OFFERTE
+    if( eta == 'minorenne'){
+        costo -= costo * 0.2;
+        offerta = 'Sconto Minorenne'
+    } else if ( eta == 'over-65'){
+        costo -= costo * 0.4;
+        offerta = 'Sconto Over 65'
+    } else {
+        offetta = 'Standard'
+    }
+
+    //COSTO FINALE
+    costo = costo.toFixed(2) + '€';
+    console.log(costo);
+
+    //CARROZZA
+    var carrozza = Math.floor(Math.random () *10) +1;
+    console.log(carrozza);
+
+    //CODICE CP
+    var codiceCp = Math.floor(Math.random () *(10000 - 9000)) +9000;
+    console.log(codiceCp);
+
+    //BIGLIETTO
+
+    //NOME PASSEGGERO
+    document.getElementById('nome-passeggero').innerHTML = nome;
+
+    //OFFERTA
+    document.getElementById('offerta').innerHTML = offerta;
+
+    //CARROZZA
+    document.getElementById('numero-carrozza').innerHTML = carrozza;
+
+    //CODICECP  
+    document.getElementById('codice-cp').innerHTML = codiceCp;
+
+    //COSTO
+    document.getElementById('costo').innerHTML = costo;
 })
 
+
+
+
+
+
+
+
+
 bottoneAnnulla.addEventListener('click', function (){
-    
+
+    document.getElementById('name').innerHTML = '';
+
+    document.getElementById('km').innerHTML = '';
+
+   
 })
